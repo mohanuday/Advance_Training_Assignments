95c4e2242c3a898087479ba1905f11d80163e34b
mohanuday <97733838+mohanuday@users.noreply.github.com>
2022-03-19 09:36:21 +0000

    Add files via upload

---
+++ Advance_Training_assignments_day_3/ProjectManagement/src/productManagement/ProductManagement.java
@@ -0,0 +1,10 @@
+package productManagement;
+import com.ui.ProductConsole;
+
+public class ProductManagement {
+	 public static void main(String[] args) {
+	        ProductConsole pc = new ProductConsole();
+	        pc.start();
+	    }
+	    
+}
